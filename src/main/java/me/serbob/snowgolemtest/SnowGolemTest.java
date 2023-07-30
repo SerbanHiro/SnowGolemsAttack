3dbd1a90c44461d7c34da4c60649a3c86e94b24e
SerbanHiro <51057957+SerbanHiro@users.noreply.github.com>
2023-07-30 20:10:22 +0300

    Metrics

--- src/main/java/me/serbob/snowgolemtest/SnowGolemTest.java
+++ src/main/java/me/serbob/snowgolemtest/SnowGolemTest.java
@@ -1,5 +1,7 @@
 package me.serbob.snowgolemtest;
 
+import me.serbob.snowgolemtest.Metrics.Metrics;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowman;
 import org.bukkit.event.EventHandler;
@@ -7,16 +9,49 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
 public final class SnowGolemTest extends JavaPlugin implements Listener {
     public static SnowGolemTest instance;
     @Override
     public void onEnable() {
         instance=this;
         saveDefaultConfig();
+        enableMetrics();
         getServer().getPluginManager().registerEvents(new ListenerTEst(), this);
     }
 
     @Override
     public void onDisable() {
+    }
+    public void enableMetrics() {
+        Metrics metrics = new Metrics(this,19317);
+        metrics.addCustomChart(new Metrics.MultiLineChart("players_and_servers", new Callable<Map<String, Integer>>() {
+            @Override
+            public Map<String, Integer> call() throws Exception {
+                Map<String, Integer> valueMap = new HashMap<>();
+                valueMap.put("servers", 1);
+                valueMap.put("players", Bukkit.getOnlinePlayers().size());
+                return valueMap;
+            }
+        }));
+        metrics.addCustomChart(new Metrics.DrilldownPie("java_version", () -> {
+            Map<String, Map<String, Integer>> map = new HashMap<>();
+            String javaVersion = System.getProperty("java.version");
+            Map<String, Integer> entry = new HashMap<>();
+            entry.put(javaVersion, 1);
+            if (javaVersion.startsWith("1.7")) {
+                map.put("Java 1.7", entry);
+            } else if (javaVersion.startsWith("1.8")) {
+                map.put("Java 1.8", entry);
+            } else if (javaVersion.startsWith("1.9")) {
+                map.put("Java 1.9", entry);
+            } else {
+                map.put("Other", entry);
+            }
+            return map;
+        }));
     }
 }
